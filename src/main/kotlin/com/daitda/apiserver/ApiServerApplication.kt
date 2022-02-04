fe4333aca7428b3c3382593082014865e209115d
youngbeom0908 <youngbeom0908@gmail.com>
2022-02-05 01:39:44 +0900

    Chore: DB 미정으로 인한 제외 설정
    - Todo: DB(MySQL)를 Docker로 올리고 연동할 수 있도록 설정

--- src/main/kotlin/com/daitda/apiserver/ApiServerApplication.kt
+++ src/main/kotlin/com/daitda/apiserver/ApiServerApplication.kt
@@ -1,11 +1,14 @@
 package com.daitda.apiserver
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = [DataSourceAutoConfiguration::class])
 class ApiServerApplication
 
 fun main(args: Array<String>) {
-	runApplication<ApiServerApplication>(*args)
+    runApplication<ApiServerApplication>(*args)
 }
